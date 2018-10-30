fa8d74d462d43c03ea8296d0d924b33cfc2ff7b3
Dai Nguyen <ngocdai94@gmail.com>
2018-10-29 20:45:56 -0700

    Updated some command lines

--- my_sript.sh
+++ my_sript.sh
@@ -2,3 +2,15 @@
 
 echo "hello ITC-136"
 echo "Edited in github"
+
+echo""
+echo "This is your current working directory"
+pwd
+
+echo ""
+echo "This is the version of lnux you're running"
+uname -a
+
+echo ""
+echo "These are your logged in users:"
+who
