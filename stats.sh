06316967fb42e0676c0eab307057f73afb6ed414
Dai Nguyen <ngocdai94@gmail.com>
2018-11-19 19:40:12 -0800

    Update stats.sh

--- stats.sh
+++ stats.sh
@@ -14,13 +14,16 @@ echo ""
 
 echo -e "\e[36m how many connections there are to the current machine?"
 netstat
+
+#to count: netstat | grep "CONNECTED" | wc -l
+# "wc -l" is the function to count
+
 echo ""
 echo ""
 echo ""
  
 echo -e "\e[36m who is logged in?"
 who 
-
 echo ""
 echo ""
 echo ""
