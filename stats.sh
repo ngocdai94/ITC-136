aadc195fc8eff390068c1e3163717c9be8bcf3eb
Dai Nguyen <ngocdai94@gmail.com>
2018-11-19 20:30:58 -0800

    Update stats.sh

--- stats.sh
+++ stats.sh
@@ -10,14 +10,21 @@ memfree=$(free -mh | grep Mem: | awk '{print $7}')
 connections=$(netstat | grep tcp)
 
 #Logged In Check?
-users=$(who)
+users=$(who | awk '{print $1}')
 
-echo "This is a snapshot of your current of system:
-                                  Disk Used: $diskused
-                                  Free Memory:$memfree
-                                  Logged in Users: $users
+# "-en / -e" interpret color
+echo -en "This is a snapshot of your current of system:
+         \e[92mLight green                         Disk Used: $diskused
+         \e[91mLight red                           Free Memory: $memfree
+         \e[96mLight cyan                          Logged in Users: $users
                 
                 
 Open Internet Connections:
 $connections
 "
+
+#Color Formating in Bash:
+#http://misc.flogisoft.com/bash/tip_colors_and_formatting (Links to an external site.)Links to an external site.
+#http://wiki.bash-hackers.org/scripting/style (Links to an external site.)
+
+
